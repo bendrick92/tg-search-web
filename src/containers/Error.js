c16e3ee9c7eb8bf436b50d48eed4c2cf9af2f472
Ben Walters <ben@Bens-Air.lan>
2023-06-28 21:44:20 -0500

    Cleaned up metadata, manifest.json, prettified 404 and 500 error pages

--- src/containers/Error.js
+++ src/containers/Error.js
@@ -1,6 +1,17 @@
+import {AttentionSeeker} from 'react-awesome-reveal';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBomb} from '@fortawesome/free-solid-svg-icons';
+
 const Error = () => {
   return (
-    <div>Oops something went wrong!</div>
+    <div className='w-full min-h-full grow flex flex-col items-center justify-center gap-5'>
+      <h1 className='text-3xl upper font-bold'>500 Server Error</h1>
+      <AttentionSeeker effect='wobble'>
+        <FontAwesomeIcon icon={faBomb} size='4x' className='text-gray-300'/>
+      </AttentionSeeker>
+      <h2 className='text-md italic'>Oops, something went kaboom!</h2>
+      <div>Please refresh the page or go back and try again.</div>
+    </div>
   );
 };
 
--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,6 +1,16 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {AttentionSeeker} from 'react-awesome-reveal';
+
 const NotFound = () => {
   return (
-    <div>Oops not found!</div>
+    <div className='w-full min-h-full grow flex flex-col items-center justify-center gap-5'>
+      <h1 className='text-3xl upper font-bold'>404 Not Found</h1>
+      <AttentionSeeker effect='wobble'>
+        <FontAwesomeIcon icon={faSearch} size='2x' className='text-gray-300'/>
+      </AttentionSeeker>
+      <h2 className='text-md italic'>These are not the droids you're looking for...</h2>
+    </div>
   );
 };
 
