89dac745e1faaf15758bbab7cf76bfee86e7af4e
Ben Walters <ben@Bens-Air.lan>
2023-06-28 21:48:51 -0500

    Added cleaner styling to error pages text

--- src/containers/Error.js
+++ src/containers/Error.js
@@ -5,12 +5,12 @@ import {faBomb} from '@fortawesome/free-solid-svg-icons';
 const Error = () => {
   return (
     <div className='w-full min-h-full grow flex flex-col items-center justify-center gap-5'>
-      <h1 className='text-3xl upper font-bold'>500 Server Error</h1>
+      <h1 className='text-3xl'><span className='font-black'>500</span> <span className='font-light'>Not Found</span></h1>
       <AttentionSeeker effect='wobble'>
         <FontAwesomeIcon icon={faBomb} size='4x' className='text-gray-300'/>
       </AttentionSeeker>
       <h2 className='text-md italic'>Oops, something went kaboom!</h2>
-      <div>Please refresh the page or go back and try again.</div>
+      <div className='font-light'>Please refresh the page or go back and try again.</div>
     </div>
   );
 };
--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -5,11 +5,11 @@ import {AttentionSeeker} from 'react-awesome-reveal';
 const NotFound = () => {
   return (
     <div className='w-full min-h-full grow flex flex-col items-center justify-center gap-5'>
-      <h1 className='text-3xl upper font-bold'>404 Not Found</h1>
+      <h1 className='text-3xl'><span className='font-black'>404</span> <span className='font-light'>Not Found</span></h1>
       <AttentionSeeker effect='wobble'>
         <FontAwesomeIcon icon={faSearch} size='2x' className='text-gray-300'/>
       </AttentionSeeker>
-      <h2 className='text-md italic'>These are not the droids you're looking for...</h2>
+      <h2 className='text-md italic font-light'>These are not the cars you're looking for...</h2>
     </div>
   );
 };
